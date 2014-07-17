97e412a320febf01056657e05bc4630b3125aa9f
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-07-17 23:42:46 +1000

    PUPPET-133 Review test

--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -4,7 +4,7 @@ describe 'munin::plugin' do
   let(:title) { 'pluginA' }
   let(:params) { {:name => 'pluginA'} }
 
-  it 'creates link to plugin' do
+  specify 'creates link to plugin' do
     should contain_file('/etc/munin/plugins/pluginA').with({
       'ensure' => 'link',
       'target' => '/usr/share/munin/plugins/pluginA',
