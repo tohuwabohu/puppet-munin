5a91d95fa9352cb27e0d10e7633f1ab1d8d2eba5
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-07-17 23:24:31 +1000

    PUPPET-133 Review tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,37 +3,37 @@ require 'spec_helper'
 describe 'munin' do
   let(:title) { 'munin' }
 
-  describe 'standard installation' do
+  describe 'by default' do
     let(:params) { { } }
 
-    it { should contain_file('/etc/munin/munin.conf') }
-    it { should contain_file('/etc/munin/munin-node.conf') }
-    it { should contain_package('munin').with_ensure('latest') }
-    it { should contain_package('munin-node').with_ensure('latest') }
-    it { should contain_service('munin-node').with_ensure('running') }
+    specify { should contain_file('/etc/munin/munin.conf') }
+    specify { should contain_file('/etc/munin/munin-node.conf') }
+    specify { should contain_package('munin').with_ensure('latest') }
+    specify { should contain_package('munin-node').with_ensure('latest') }
+    specify { should contain_service('munin-node').with_ensure('running') }
   end
 
-  describe 'custom version' do
+  describe 'with version => 1.0.0' do
     let(:params) { {:version => '1.0.0' } }
 
-    it { should contain_package('munin').with_ensure('1.0.0') }
-    it { should contain_package('munin-node').with_ensure('1.0.0') }
+    specify { should contain_package('munin').with_ensure('1.0.0') }
+    specify { should contain_package('munin-node').with_ensure('1.0.0') }
   end
 
-  describe 'disable service' do
+  describe 'with disable => true' do
     let(:params) { {:disable => true } }
 
-    it { should contain_file('/etc/munin/munin.conf') }
-    it { should contain_file('/etc/munin/munin-node.conf') }
-    it { should contain_package('munin').with_ensure('latest') }
-    it { should contain_package('munin-node').with_ensure('latest') }
-    it { should contain_service('munin-node').with_ensure('stopped').with_enable(false) }
+    specify { should contain_file('/etc/munin/munin.conf') }
+    specify { should contain_file('/etc/munin/munin-node.conf') }
+    specify { should contain_package('munin').with_ensure('latest') }
+    specify { should contain_package('munin-node').with_ensure('latest') }
+    specify { should contain_service('munin-node').with_ensure('stopped').with_enable(false) }
   end
 
-  describe 'manages plugins' do
+  describe 'with plugins => [pluginA, pluginB]' do
     let(:params) { {:plugins => ['pluginA', 'pluginB'] } }
 
-    it { should contain_munin__plugin('pluginA') }
-    it { should contain_munin__plugin('pluginB') }
+    specify { should contain_munin__plugin('pluginA') }
+    specify { should contain_munin__plugin('pluginB') }
   end
 end
