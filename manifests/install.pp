dd548c4c12db8fc10e08000e566f6662fc502402
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-07-17 23:19:06 +1000

    PUPPET-133 Actually add install manifest

---
+++ manifests/install.pp
@@ -0,0 +1,22 @@
+# = Class: munin::install
+#
+# Install the munin package.
+#
+# == Author
+#
+#   Martin Meinhold <Martin.Meinhold@gmx.de>
+#
+# === Copyright
+#
+# Copyright 2014 Martin Meinhold, unless otherwise noted.
+#
+class munin::install inherits munin {
+  $packages = [
+    $munin::params::master_package_name,
+    $munin::params::node_package_name
+  ]
+
+  package { $packages:
+    ensure => $munin::version
+  }
+}
