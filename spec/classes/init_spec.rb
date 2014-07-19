01e392e82ae40e23a95dcdd2268db5a02f5911f5
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-07-19 15:25:00 +1000

    PUPPET-133 Rename test fixture

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe 'munin' do
   let(:title) { 'munin' }
-  let(:master_conf_file) { '/etc/munin/munin.conf' }
-  let(:node_conf_file) { '/etc/munin/munin-node.conf' }
+  let(:master_config_file) { '/etc/munin/munin.conf' }
+  let(:node_config_file) { '/etc/munin/munin-node.conf' }
   let(:facts) { {:fqdn => 'node.example.com' } }
 
   describe 'by default' do
     let(:params) { { } }
 
-    specify { should contain_file(master_conf_file) }
-    specify { should contain_file(node_conf_file) }
-    specify { should contain_file(node_conf_file).with_content(/host_name node.example.com/) }
-    specify { should contain_file(node_conf_file).with_content(/timeout 60/) }
+    specify { should contain_file(master_config_file) }
+    specify { should contain_file(node_config_file) }
+    specify { should contain_file(node_config_file).with_content(/host_name node.example.com/) }
+    specify { should contain_file(node_config_file).with_content(/timeout 60/) }
     specify { should contain_package('munin').with_ensure('installed') }
     specify { should contain_package('munin-node').with_ensure('installed') }
     specify { should contain_service('munin-node').with_ensure('running') }
@@ -28,8 +28,8 @@ describe 'munin' do
   describe 'with ensure => absent' do
     let(:params) { {:ensure => 'absent' } }
 
-    specify { should contain_file(master_conf_file).with_ensure('absent') }
-    specify { should contain_file(node_conf_file).with_ensure('absent') }
+    specify { should contain_file(master_config_file).with_ensure('absent') }
+    specify { should contain_file(node_config_file).with_ensure('absent') }
     specify { should contain_package('munin').with_ensure('absent') }
     specify { should contain_package('munin-node').with_ensure('absent') }
     specify { should_not contain_service('munin-node') }
@@ -38,8 +38,8 @@ describe 'munin' do
   describe 'with enable => false' do
     let(:params) { {:enable => false } }
 
-    specify { should contain_file(master_conf_file) }
-    specify { should contain_file(node_conf_file) }
+    specify { should contain_file(master_config_file) }
+    specify { should contain_file(node_config_file) }
     specify { should contain_package('munin') }
     specify { should contain_package('munin-node') }
     specify { should contain_service('munin-node').with_ensure('stopped').with_enable(false) }
@@ -49,7 +49,7 @@ describe 'munin' do
     let(:params) { {:master_config_template => ''} }
 
     specify do
-      expect { should contain_file(master_conf_file) }.to raise_error(Puppet::Error, /master_config_template/)
+      expect { should contain_file(master_config_file) }.to raise_error(Puppet::Error, /master_config_template/)
     end
   end
 
@@ -57,7 +57,7 @@ describe 'munin' do
     let(:params) { {:node_config_template => ''} }
 
     specify do
-      expect { should contain_file(node_conf_file) }.to raise_error(Puppet::Error, /node_config_template/)
+      expect { should contain_file(node_config_file) }.to raise_error(Puppet::Error, /node_config_template/)
     end
   end
 
@@ -65,21 +65,21 @@ describe 'munin' do
     let(:params) { {:node_hostname => ''} }
 
     specify do
-      expect { should contain_file(node_conf_file) }.to raise_error(Puppet::Error, /node_hostname/)
+      expect { should contain_file(node_config_file) }.to raise_error(Puppet::Error, /node_hostname/)
     end
   end
 
   describe 'should accept custom node_hostname' do
     let(:params) { {:node_hostname => 'beaf.example.com'} }
 
-    specify { should contain_file(node_conf_file).with_content(/host_name beaf.example.com/) }
+    specify { should contain_file(node_config_file).with_content(/host_name beaf.example.com/) }
   end
 
   describe 'should not accept empty node_timeout' do
     let(:params) { {:node_timeout => ''} }
 
     specify do
-      expect { should contain_file(node_conf_file) }.to raise_error(Puppet::Error, /node_timeout/)
+      expect { should contain_file(node_config_file) }.to raise_error(Puppet::Error, /node_timeout/)
     end
   end
 
@@ -87,14 +87,14 @@ describe 'munin' do
     let(:params) { {:node_timeout => 'invalid'} }
 
     specify do
-      expect { should contain_file(node_conf_file) }.to raise_error(Puppet::Error, /node_timeout/)
+      expect { should contain_file(node_config_file) }.to raise_error(Puppet::Error, /node_timeout/)
     end
   end
 
   describe 'should accept custom node_timeout' do
     let(:params) { {:node_timeout => 120} }
 
-    specify { should contain_file(node_conf_file).with_content(/timeout 120/) }
+    specify { should contain_file(node_config_file).with_content(/timeout 120/) }
   end
 
   describe 'with plugins => [pluginA, pluginB]' do
